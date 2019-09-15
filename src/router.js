89f3d662190ee0f99095990246afb8dd766d0a06
chenyx <18271002913@163.com>
2019-09-15 14:38:43 +0800

    完成用户显示、添加、删除与修改功能

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from './components/login.vue'
 import home from './components/home.vue'
+import welcome from './components/welcome.vue'
+import users from './components/user/users.vue'
 
 Vue.use(Router)
 
@@ -9,18 +11,27 @@ const router = new Router({
     routes: [
         { path: '/', redirect: '/login' },
         { path: '/login', component: login },
-        { path: '/home', component: home }
+        {
+            path: '/home',
+            component: home,
+            redirect: '/welcome',
+            children: [
+                { path: '/welcome', component: welcome },
+                { path: '/users', component: users }
+            ]
+        }
     ]
 })
 
-//挂载路由导航守卫
+// 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
     // to 将要访问的路径
     // from 代表从哪个路径跳转而来
     // next 是一个函数，表示放行
-    // next()  放行    next('/login')  强制跳转
+    //     next()  放行    next('/login')  强制跳转
+
     if (to.path === '/login') return next()
-        //获取token
+        // 获取token
     const tokenStr = window.sessionStorage.getItem('token')
     if (!tokenStr) return next('/login')
     next()
